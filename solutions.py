6c63008b3d93bf81e331e7df9c7241a79bc31074
Natalya Rapstine <nrapstine@usgs.gov>
2017-03-26 15:16:43 -0600

    add test cases for each question and space efficiency in the write-up

--- solutions.py
+++ solutions.py
@@ -9,32 +9,34 @@ def question1(s, t):
     # anagrams of t
     t_anagrams = [list(x) for x in permutations(t, len(t) ) ]
     anagram = ["".join(x) for x in t_anagrams ]
-    print anagram
+    # print anagram
+    if t == "":
+        return False
+    else:
+        # check if t is in s:
+        for i in anagram:
 
-    # check if t is in s:
-    for i in anagram:
+            if i in s:
+                return True
 
-        if i in s:
-            return True
-
-    return False
+        return False
 
 # Test
+print question1("udacity", "ad")
 # Should return True
-# print question1("udacity", "ad")
-#
-# # Should return False
-# print question1("udacity", "g")
+print question1("udacity", "g")
+# Should return False
+print question1("udacity", "")
+# Should return False
+
 
 '''Question 2'''
 def question2(a) :
     '''Given a string a,
     find the longest palindromic substring contained in a.'''
-    palydrome = ''
+    palindrome = ''
     a_list = list(a)
-
     for i in range(0, len(a)-1):
-
         i_list = a_list[i:]
 
         # dummy lists
@@ -57,136 +59,170 @@ def question2(a) :
                     break
                 else:
                     # Check if new palindrom is longer than the last
-                    if len(palydrome) < len(''.join(each) ) :
-                        palydrome = ''.join(each)
+                    if len(palindrome) < len(''.join(each) ) :
+                        palindrome = ''.join(each)
 
-    return palydrome
-
+    return palindrome
 
 # Test
-# print question2("93138")
-# print question2("696531358")
+print question2("93138")
+# Should return 313
+print question2("696531358")
+# Should return 53135
+print question2("")
+# Should return ""
+
 
 '''Question 3'''
 def question3(G):
     '''Given an undirected graph G, find the minimum spanning tree within G.'''
-    # Array to keep track of known nodes
-    known = [False for i in range(len(G))]
-    # Array to keep track of weights
-    cost = [float('Inf') for i in range(len(G))]
-    # Path to assemble the minimum cost spanning tree
-    path = [-1 for i in range(len(G))]
+    if G == None or G == {}:
+        return None
+    else:
 
-    vertex = G.keys()
-    vertex = sorted(vertex)   # sort alphabetically
-    cost[0] = 0
+        # Array to keep track of known nodes
+        known = [False for i in range(len(G))]
+        # Array to keep track of weights
+        cost = [float('Inf') for i in range(len(G))]
+        # Path to assemble the minimum cost spanning tree
+        path = [-1 for i in range(len(G))]
 
-    index = 0  # start with 'A' node
-    while (False in known) :
+        vertex = G.keys()
+        vertex = sorted(vertex)   # sort alphabetically
+        cost[0] = 0
 
-        known[index] = True
-        # Update neighbors
-        for v in G[vertex[index]]:
-            if known[vertex.index(v[0])] == False and cost[vertex.index(v[0])] > v[1]:
-                cost[vertex.index(v[0])] = v[1]
-                path[vertex.index(v[0])] = index
+        index = 0  # start with 'A' node
+        while (False in known) :
 
-        # Find cheapest unknown vertex
-        minvalue = float('Inf')
-        for i in range(len(known)):
-            if known[i] == False and cost[i] < float('Inf'):
-                if minvalue > cost[i] :
-                    minvalue = cost[i]
-                    index = i
+            known[index] = True
+            # Update neighbors
+            for v in G[vertex[index]]:
+                if known[vertex.index(v[0])] == False and cost[vertex.index(v[0])] > v[1]:
+                    cost[vertex.index(v[0])] = v[1]
+                    path[vertex.index(v[0])] = index
 
-    # print 'cost updated', cost
-    # print 'vertex array', vertex
-    # print 'path ', path
+            # Find cheapest unknown vertex
+            minvalue = float('Inf')
+            for i in range(len(known)):
+                if known[i] == False and cost[i] < float('Inf'):
+                    if minvalue > cost[i] :
+                        minvalue = cost[i]
+                        index = i
 
-    # Write out the minimum spanning tree
-    Ans = {}
-    for i, v in enumerate(vertex):
-        tmp = []
-        for j, p in enumerate(path):
-            if p == i:
-                tmp.append( (vertex[j], cost[j] ) )
+        # print 'cost updated', cost
+        # print 'vertex array', vertex
+        # print 'path ', path
 
-                # Check if node is already in the dictionary
-                if vertex[j] in Ans:
-                    Ans[vertex[j]].append( ( v, cost[j] ))
-                else:
-                    Ans[vertex[j]] = [(v, cost[j])]
+        # Write out the minimum spanning tree
+        Ans = {}
+        for i, v in enumerate(vertex):
+            tmp = []
+            for j, p in enumerate(path):
+                if p == i:
+                    tmp.append( (vertex[j], cost[j] ) )
 
-        # Check if node is already in the dictionary
-        if v in Ans :
-            if tmp != []:
-                Ans[v].extend( tmp )
-        else:
-            Ans[v] = tmp
-        # raw_input("Press Enter to continue...")
+                    # Check if node is already in the dictionary
+                    if vertex[j] in Ans:
+                        Ans[vertex[j]].append( ( v, cost[j] ))
+                    else:
+                        Ans[vertex[j]] = [(v, cost[j])]
 
-    return Ans
+            # Check if node is already in the dictionary
+            if v in Ans :
+                if tmp != []:
+                    Ans[v].extend( tmp )
+            else:
+                Ans[v] = tmp
+            # raw_input("Press Enter to continue...")
+
+        return Ans
 
 # Test
-# G = {'A': [('D', 2), ('E', 4)],
-#      'B': [('D', 6), ('F', 3)],
-#      'C': [('F', 4), ('G', 6)],
-#      'D': [('A', 2), ('B', 6)],
-#      'E': [('A', 4), ('G', 2), ('H', 2)],
-#      'F': [('B', 3), ('C', 4), ('G', 4)],
-#      'G': [('C', 6), ('E', 2), ('F', 4)],
-#      'H': [('E', 2)]}
-#
-# print question3(G)
+G = {'A': [('B', 2)],
+     'B': [('A', 2), ('C', 5)],
+     'C': [('B', 5)]}
+print question3(G)
+# Should return {'A': [('B', 2)],
+#                'B': [('A', 2), ('C', 5)],
+#                'C': [('B', 5)]}
+
+
+G = {'A': [('D', 2), ('E', 4)],
+     'B': [('D', 6), ('F', 3)],
+     'C': [('F', 4), ('G', 6)],
+     'D': [('A', 2), ('B', 6)],
+     'E': [('A', 4), ('G', 2), ('H', 2)],
+     'F': [('B', 3), ('C', 4), ('G', 4)],
+     'G': [('C', 6), ('E', 2), ('F', 4)],
+     'H': [('E', 2)]}
+
+print question3(G)
+# Should return {'A': [('D', 2), ('E', 4)],
+#                'B': [('F', 3)],
+#                'C': [('F', 4)],
+#                'D': [('A', 2)],
+#                'E': [('A', 4), ('G', 2), ('H', 2)],
+#                'F': [('B', 3), ('C', 4), ('G', 4)],
+#                'G': [('E', 2), ('F', 4)],
+#                'H': [('E', 2)]}
+
+print question3(None)
 
 '''Question 4'''
 def question4(T, r, n1, n2):
     '''Find the least common ancestor between two nodes
      on a binary search tree.'''
-    n1_list = []
-    n2_list = []
-    while n1 != n2:
 
-        for i in range(len(T)):
-            for ii, v in enumerate(T[i]):
+    if T == []:
+        return "Input is an empty tree"
+    else:
+        n1_list = []
+        n2_list = []
+        while n1 != n2:
 
-                # find parents of n1 and n2
-                if ii == n1 and v == 1:
-                    n1 = i
-                    n1_list.append(n1)
-                if ii == n2 and v == 1:
-                    n2 = i
-                    n2_list.append(n2)
+            for i in range(len(T)):
+                for ii, v in enumerate(T[i]):
+                    # find parents of n1 and n2
+                    if ii == n1 and v == 1:
+                        n1 = i
+                        n1_list.append(n1)
+                    if ii == n2 and v == 1:
+                        n2 = i
+                        n2_list.append(n2)
 
-    # Search for least common ancestor
-    for n in n1_list:
-        if n in n2_list:
-            n1 = n
-            break
-    return n1
+        # Search for least common ancestor
+        for n in n1_list:
+            if n in n2_list:
+                n1 = n
+                break
+        return n1
 
 # Test
-# print "Should be 3"
-# print question4([[0, 1, 0, 0, 0],
-#                 [0, 0, 1, 0, 0],
-#                 [0, 0, 0, 0, 0],
-#                 [1, 0, 0, 0, 1],
-#                 [0, 0, 0, 0, 0]],
-#                 3,
-#                 1,
-#                 4)
+print question4([[0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0]],
+                3,
+                1,
+                4)
+# Should return 3
 
-# print question4([[0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 0, 0, 0, 0, 0],
-#                  [1, 0, 0, 0, 1, 0, 0, 0],
-#                  [0, 1, 0, 0, 0, 0, 0, 0],
-#                  [0, 0, 0, 1, 0, 0, 1, 0],
-#                  [0, 0, 1, 0, 0, 0, 0, 1],
-#                  [0, 0, 0, 0, 0, 0, 0, 0]],
-#                  5,
-#                  0,
-#                  4)
+print question4([[0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0],
+                 [1, 0, 0, 0, 1, 0, 0, 0],
+                 [0, 1, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0, 0, 1, 0],
+                 [0, 0, 1, 0, 0, 0, 0, 1],
+                 [0, 0, 0, 0, 0, 0, 0, 0]],
+                 5,
+                 6,
+                 2)
+# Should return 2
+
+print question4([], 1, 0, 0)
+# Should return "Input is an empty tree"
 
 '''Question 5'''
 class Node(object):
@@ -194,7 +230,10 @@ class Node(object):
     self.data = data
     self.next = None
 
-def question5(ll, m):
+# def question5(ll, m):
+# For testing, pass a linked list as an argument
+def question5(ll, m, list):
+
     '''Finds the element in a singly linked list that's m elements from the end.
     Return the value of the node at that position.'''
 
@@ -204,39 +243,43 @@ def question5(ll, m):
         stack.append(current.data)
         current = current.next
     stack.append(current.data)
+    if m > len(stack):
+        return "Not a valid input. Choose a smaller m value."
 
     return stack[ len(stack) - m ]
 
 # Test
 # For testing, create a linked list class
-# class LinkedList(object):
-#     def __init__(self, head=None):
-#         self.head = head
-#
-#     def append(self, new_element):
-#         current = self.head
-#         if self.head:
-#             while current.next:
-#                 current = current.next
-#             current.next = new_element
-#         else:
-#             self.head = new_element
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
 
-# And pass a linked list as an argument
-# def question5(ll, m, list):
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
 
 # Nodes to append to a linked list
-# n1 = Node(5)
-# n2 = Node(3)
-# n3 = Node(2)
-# n4 = Node(1)
-# n5 = Node(0)
-#
-# # Linked List
-# linked = LinkedList(n1)
-# linked.append(n2)
-# linked.append(n3)
-# linked.append(n4)
-# linked.append(n5)
-#
-# print question5(n1, 2, linked)
+n1 = Node(5)
+n2 = Node(3)
+n3 = Node(2)
+n4 = Node(1)
+n5 = Node(0)
+
+# Linked List
+linked = LinkedList(n1)
+linked.append(n2)
+linked.append(n3)
+linked.append(n4)
+linked.append(n5)
+
+print question5(n1, 2, linked)
+# Should return 1
+print question5(n1, 5, linked)
+# Should return 5
+print question5(n1, 6, linked)
+# Should return "Not a valid input. Choose a smaller m value."
